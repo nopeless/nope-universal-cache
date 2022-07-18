43a64fbf3067d5974ee8ab72b08e8b2b7cb17bb4
nopeless <38830903+nopeless@users.noreply.github.com>
2022-07-18 22:18:34 +0900

    delete unused file

--- what.js
+++
@@ -1,9 +0,0 @@
-class MyConstants {
-  get foo() {
-    return `foo`;
-  }
-}
-
-console.log(MyConstants.foo); // 'foo'
-MyConstants.foo = `bar`;
-console.log(MyConstants.foo); // 'foo', a static getter's value cannot be changed
