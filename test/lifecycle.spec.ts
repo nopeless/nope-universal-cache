30ad361cda9ddb08e9c5a0d5da8736fce3c426d6
nopeless <38830903+nopeless@users.noreply.github.com>
2022-07-18 21:36:48 +0900

    improve tests

--- test/lifecycle.spec.ts
+++ test/lifecycle.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import {
   CacheManager,
+  CacheManagerPromise,
   IndependentStoreCacheWrapper,
   MemoryStore,
 } from "../src/index.js";
@@ -18,9 +19,35 @@ describe(`CacheManager`, function () {
       }),
     ]);
 
+    expect(await c.get(`foo`)).to.equal(undefined);
     await c.set(`foo`, `bar`);
+    expect(c.get(`foo`)).to.be.a.instanceof(Promise);
     expect(await c.get(`foo`)).to.equal(`bar`);
     await sleep(10);
     expect(await c.get(`foo`)).to.equal(undefined);
+  });
+  it(`should work (promise)`, async function () {
+    const c = new CacheManagerPromise<string>([
+      new IndependentStoreCacheWrapper(new MemoryStore(), {
+        ttl: `5ms`,
+      }),
+    ]);
+
+    expect(await c.get(`foo`)).to.equal(undefined);
+    await c.set(`foo`, `bar`);
+    expect(c.get(`foo`)).to.be.a.instanceof(Promise);
+    expect(await c.get(`foo`)).to.equal(`bar`);
+    await sleep(10);
+    expect(await c.get(`foo`)).to.equal(undefined);
+
+    await c.set(
+      `foo`,
+      (async () => {
+        await sleep(2);
+        return `bar`;
+      })()
+    );
+
+    expect(await c.get(`foo`)).to.equal(`bar`);
   });
 });
